e0ca334a1dcd6fee96aa4df9ff77bbd348e879f2
Alan Lachkar <alan.lachkar@hotmail.fr>
2021-05-09 16:56:41 +0200

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+
+#############################
+# docker command used in this file is the same that the one used in travis.yml file
+# Build all our images with 2 tags: 
+# - one being the latest 
+# - another being a specific version number corresponding to the last commit id
+docker build -t alanlachkar/multi-client:latest -t alanlachkar/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t alanlachkar/multi-server:latest -t alanlachkar/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t alanlachkar/multi-worker:latest -t alanlachkar/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+# Push each to Docker Hub
+docker push alanlachkar/milti-client:latest
+docker push alanlachkar/milti-server:latest
+docker push alanlachkar/milti-worker:latest
+
+docker push alanlachkar/milti-client:$SHA
+docker push alanlachkar/milti-server:$SHA
+docker push alanlachkar/milti-worker:$SHA
+
+#############################
+# kubectl command used in this file is the same that the one used in travis.yml file
+# Apply all configuration files in "k8s" folder
+kubectl apply -f k8s
+
+#############################
+# Imperative Kubernetes command set images on each deployment with a specific version
+kubectl set image deployments/server-deployment server=alanlachkar/multi-server:$SHA
+kubectl set image deployments/client-deployment client=alanlachkar/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=alanlachkar/multi-worker:$SHA
